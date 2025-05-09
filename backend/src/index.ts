2b7927e50e3b8e7261125c6006cd5dc3709b0e58
Aiz <alt.then.aiz@gmail.com>
2025-05-09 00:11:36 -0700

    Initial commit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route";
 import userRoutes from "./routes/user.route";
@@ -14,22 +14,31 @@ app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-const allowedOrigins = ['http://localhost:5173', 'https://jeffanians-baitrack.vercel.app', '*'];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://jeffanians-baitrack.vercel.app",
+];
 
-app.use(cors({
-  origin: 'http://localhost:5173', 
-  credentials: true
-}));
-
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 
-
-app.get('/', (_req, res) => {
-    res.send("Server is running");
+app.get("/", (_req, res) => {
+  res.send("Server is running");
 });
 
 app.listen(PORT, () => {
-    console.log('Server is running on port ', PORT);
+  console.log("Server is running on port ", PORT);
 });
